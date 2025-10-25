5969326f80cfb5b0e70259edef08f439afea76a4
MaxiCode <andresholsbach@gmail.com>
2025-10-25 02:10:19 -0300

    Se Comienza con modelado de ECOMERCE

---
+++ InterfacesExcepciones/src/Ecommerce/TarjetaCredito.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ecommerce;
+
+/**
+ *
+ * @author Maxi
+ */
+public class TarjetaCredito implements PagoConDescuento {
+    private String nombre;
+    private String numeroTarjeta;
+    private Cliente titular;
+
+    @Override
+    public double aplicarDescuento(double monto, double descuento) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public double procesarPago(double monto) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public double calcularTotal() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
